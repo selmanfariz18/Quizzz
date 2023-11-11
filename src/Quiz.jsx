5dcfd49aae0c19370b85133e8846d5563a5eba7b
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-11-11 22:35:39 +0530

    basic functions completed

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -43,6 +43,11 @@ const Quiz = ({ questions }) => {
 
   };
 
+  const onTryAgain = () => {
+    setResult(resultInitialState);
+    setShowResult(false);
+  }
+
   return (
     <div className="quiz_container">
       {!showResult ? (      <>
@@ -79,7 +84,7 @@ const Quiz = ({ questions }) => {
         <p>
             Wrong Answers : <span>{result.wrongAnswers}</span>
         </p>
-        <button>Try again</button>
+        <button onClick={onTryAgain}>Try again</button>
         </div>
       }
 
